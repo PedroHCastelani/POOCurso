a55513f39940d618a4f053da26de5343f09a82ea
Pedro H. Castelani <pedro.castelani@hotmail.com>
2022-04-27 10:01:10 -0300

    Acrescentei alguns métodos e comportamentos ao projeto Notebook.

--- src/Notebook/Notebook.java
+++ src/Notebook/Notebook.java
@@ -1,8 +1,47 @@
 package Notebook;
 
+import java.util.Scanner;
+
 public class Notebook {
 
         String processador;
-        int memoria;
+        String marca;
+        String HD;
+        String memoria;
+
+        void ligando(){
+            System.out.println("=============================");
+            System.out.println("Computador sendo inicializado");
+            System.out.println("=============================");
+        }
+
+        void escolherNB(){
+           Scanner leia = new Scanner(System.in);
+           int resposta;
+
+                System.out.println("Escolha qual Notebook quer inicializar: [1/2]");
+                resposta = leia.nextInt();
+
+
+           if (resposta == 1){
+                   modeloDell();
+           } else {
+                 modeloHP();
+           }
+        }
+
+        void modeloDell(){
+            System.out.println(marca = "Dell");
+            System.out.println(processador = "Core I5");
+            System.out.println(memoria = "8GB RAM");
+            System.out.println(HD = "1T");
+        }
+
+        void modeloHP(){
+            System.out.println(marca = "HP");
+            System.out.println(processador = "Core I7");
+            System.out.println(memoria = "12GB RAM");
+            System.out.println(HD = "500GB");
+        }
 
 }
--- src/Notebook/UsaNotebook.java
+++ src/Notebook/UsaNotebook.java
@@ -5,24 +5,10 @@ import java.util.Scanner;
 public class UsaNotebook {
     public static void main(String[] args){
 
-        int quantidade;
+        Notebook notebook = new Notebook();
 
-        Scanner leia = new Scanner(System.in);
-        Notebook dell = new Notebook();
-        dell.processador = "Core I5";
-        dell.memoria = 8;
+        notebook.ligando();
+        notebook.escolherNB();
 
-
-        System.out.println("Dell: ");
-        System.out.println("Processador: " + dell.processador);
-        System.out.println("Memoria: " + dell.memoria + "RAM");
-
-        Notebook hp = new Notebook();
-        hp.processador = "Xeon Extreme";
-        hp.memoria = 12;
-
-        System.out.println("\nHP: ");
-        System.out.println("Processador: " + dell.processador);
-        System.out.println("Memoria: " + dell.memoria + "RAM");
     }
 }
